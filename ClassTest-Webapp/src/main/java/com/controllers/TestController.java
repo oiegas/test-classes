4d8352cfbaea87f16278e5fcea816b9d1fda714d
AlexandruOi <AlexandruOi@AlexandruOi-lap.yonder.local>
2016-02-26 11:20:24 +0200

    new things

--- ClassTest-Webapp/src/main/java/com/controllers/TestController.java
+++ ClassTest-Webapp/src/main/java/com/controllers/TestController.java
@@ -1,14 +1,21 @@
 package com.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.converter.QuestionConverter;
+import com.converter.QuestionConverterImplementation;
 import com.converter.TestConverter;
 import com.converter.TestConverterImplementation;
+import com.domain.Question;
 import com.domain.Test;
+import com.ui.domain.UIQuestion;
 import com.ui.domain.UITest;
 
 @Controller
@@ -16,19 +23,28 @@ import com.ui.domain.UITest;
 public class TestController {
 	
 	TestConverter converter=new TestConverterImplementation();
+	QuestionConverter questionConverter=new QuestionConverterImplementation();
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView choosePersons(){
-		ModelAndView model = new ModelAndView();
-		model.setViewName("test");
-		return model;
+	public String listTest(Model model){
+		model.addAttribute("testAdd",new UITest());
+		return "test";
 	}
 	
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String addPerson(@ModelAttribute("testAdd") UITest pui) {
+	public String addPerson(@ModelAttribute("test") UITest pui, @ModelAttribute("firstQuestion") UIQuestion question) {
 		Test p = converter.createTest(pui);
+		Question q=questionConverter.createQuestion(question);
 		
 		return "redirect:/test";
+
+	}
+
+	@RequestMapping("/search")
+	public String searchPerson(@ModelAttribute("testAdd") UITest person, Model model) {
+		model.addAttribute("testAdd", new UITest());
+		
+		return "test";
 
 	}
 }
---
+++ ClassTest-Webapp/src/main/java/com/ui/domain/UICompleteTest.java
@@ -0,0 +1,17 @@
+package com.ui.domain;
+
+public class UICompleteTest {
+
+	public String testName;
+	public String questionOne;
+	public String questionTwo;
+	public String questionThree;
+	public String questionFour;
+	public String questionFive;
+	public String answerOneQuestionOne;
+	public String answerTwoQuestionOne;
+	public String answerThreeQuestionOne;
+	public String answerFourQuestionOne;
+	public String answerOneQuestionTwo;
+	public String answerTwoQuestionTwo;
+}
